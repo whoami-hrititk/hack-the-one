c80857b33cf0c2c967c73c09a3df5998df99ec94
hritik <163508501+whoami-hrititk@users.noreply.github.com>
2025-06-03 17:25:18 +0000

    Update authLogin.js

--- backend/routes/authLogin.js
+++ backend/routes/authLogin.js
@@ -2,28 +2,36 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const router = express.Router();
 
+const all_users = require('../users');
+
 router.post('/login', (req, res) => {
     const username = req.body.username;
-    const pass = req.body.pass;
+    const pass = req.body.password;
     const saltRound = 10;
 
-    if(!all_users[username]){
+    const user = all_users[username];
+
+    if(!username || !pass){
+        return res.status(400).json({message:"Missing Fields", success:false});
+    }
+    if(!user){
         return res.status(404).json({message:"User Not found!", success:false});
     }
-    if(!username || !pass){
-        return res.status(400).json({msessage:"User identity/password not found!", success:false});
-    }
-    bcrypt.hash(pass, saltRound, (err, userhashPass) => {
-        if(err){
-            return res.status(500).json({message:"Unable to generate compare hash pass!", success:false});
+    
+    bcrypt.compare(pass, user.hashPass, (err, isMatch) => {
+        if (err) {
+            return res.status(500).json({ message: "Error comparing passwords", success: false });
         }
-        
-        if(userhashPass == all_users[username][hashPass]){
-            req.session.user = {username, email};
-            console.log("User Successfully logged in!");
-            res.status(200).json({message:"Logged In!", success:true});
-        }else{
-            res.status(401).json({message:"Invalid Credentials", success:false});
+
+        if (isMatch) {
+            req.session.user = {
+                username: username,
+                email: user.email
+            };
+            console.log("User successfully logged in!");
+            res.status(200).json({ message: "Logged In!", success: true});
+        } else {
+            res.status(401).json({ message: "Invalid credentials", success: false });
         }
     });
 });
